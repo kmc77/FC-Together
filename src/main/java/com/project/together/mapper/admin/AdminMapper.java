34bff61b84172e2c9261ea7e4a492e25899197f7
choegyeongmin <k_minchoi@naver.com>
2023-12-30 22:07:41 +0900

    투게더 프로젝트 도동ㅇㄹㄴㅇㄹㄴㅇㄹ운ㅇㄹ도메도메도도메ㅇㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇㅇㅇㅇㄴㅇㄹㄴㅇㄹㄴㅇㄹㅇㄴㄹㅇㄴㄹㄴㅇㄹ4

--- src/main/java/com/project/together/mapper/admin/AdminMapper.java
+++ src/main/java/com/project/together/mapper/admin/AdminMapper.java
@@ -1,9 +1,7 @@
 package com.project.together.mapper.admin;
 
-import com.project.together.domain.TableData;
-
 import java.util.List;
 
 public interface AdminMapper {
-    List<TableData> getTableData();
+
 }

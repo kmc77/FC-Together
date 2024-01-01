d983d37667f9f4ec5406e3cf2e96cbdadaffa1e0
twzt9983@naver.com <twzt9983@naver.com>
2024-01-01 23:28:49 +0900

    develop ready complete

--- src/main/java/com/project/together/mapper/TeamMapper.java
+++ src/main/java/com/project/together/mapper/TeamMapper.java
@@ -3,7 +3,9 @@ package com.project.together.mapper;
 import com.project.together.domain.TeamStaff;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface TeamMapper {
-    TeamStaff testt();
+    TeamStaff getAllTeamStaff();
 }

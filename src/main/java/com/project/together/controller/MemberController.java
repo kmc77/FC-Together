baf1fe67548f26d39763be1622014ea91c3bd92c
choegyeongmin <k_minchoi@naver.com>
2024-01-02 18:15:16 +0900

    config, jwt 패키지 / 시큐리티 파일 생성, build.gradle 시큐리티 의존성 로직 추가

--- src/main/java/com/project/together/controller/MemberController.java
+++ src/main/java/com/project/together/controller/MemberController.java
@@ -1,7 +1,25 @@
 package com.project.together.controller;
 
+import com.project.together.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/")
+    public void getMemberById() {
+
+    }
+
 }

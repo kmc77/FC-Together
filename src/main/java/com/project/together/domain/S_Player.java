2e55017379a4dfa1a0c6540c60f30b9ab0658f72
choegyeongmin <k_minchoi@naver.com>
2024-01-13 20:58:22 +0900

    Domain class add

---
+++ src/main/java/com/project/together/domain/S_Player.java
@@ -0,0 +1,12 @@
+package com.project.together.domain;
+
+import lombok.Data;
+
+@Data
+public class S_Player {
+    private int sPlayerNum;
+    private String sPlayerName;
+    private String sPlayerCapYn;
+    private String sPlayerSubcapYn;
+    private String sPlayerPosition;
+}

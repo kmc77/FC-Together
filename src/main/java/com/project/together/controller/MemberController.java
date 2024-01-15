b0ffc0fe76bbfd418da1a09221aea14d01a89c36
choegyeongmin <k_minchoi@naver.com>
2024-01-16 00:50:17 +0900

    좋아하는 선수 처리 완료

--- src/main/java/com/project/together/controller/MemberController.java
+++ src/main/java/com/project/together/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.project.together.controller;
 
+import com.project.together.domain.K5_Player;
+import com.project.together.domain.K7_Player;
 import com.project.together.domain.Member;
+import com.project.together.domain.S_Player;
 import com.project.together.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,6 +41,24 @@ public class MemberController {
     public int emailCheck(@RequestParam("memberEmail") String memberEmail) {
         return memberService.emailCheck(memberEmail);
     }
+
+    @ResponseBody
+    @GetMapping("/players")
+    public ResponseEntity<?> getPlayers(@RequestParam("selectLeague") String league) {
+        if (league.equals("K5_Player")) {
+            List<K5_Player> playerList = memberService.getK5Players();
+            return ResponseEntity.ok(playerList);
+        } else if (league.equals("K7_Player")) {
+            List<K7_Player> playerList = memberService.getK7Players();
+            return ResponseEntity.ok(playerList);
+        } else if (league.equals("S_Player")) {
+            List<S_Player> playerList = memberService.getSPlayers();
+            return ResponseEntity.ok(playerList);
+        } else {
+            return ResponseEntity.badRequest().body("Invalid league parameter");
+        }
+    }
+
 
 
     @PostMapping("/join")

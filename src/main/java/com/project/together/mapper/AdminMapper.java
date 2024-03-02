46a64b55cd1df9880555094c904f9445f16ac007
kmc77 <k_minchoi@naver.com>
2024-03-02 14:31:17 +0900

    AdminPage 작업중

--- src/main/java/com/project/together/mapper/AdminMapper.java
+++ src/main/java/com/project/together/mapper/AdminMapper.java
@@ -1,7 +1,18 @@
 package com.project.together.mapper;
 
+import com.project.together.domain.Qna;
+import com.project.together.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
 
+@Mapper
 public interface AdminMapper {
 
+    List<User> getAllUsers();
+
+    List<Qna> getAllQnAs();
+
+    Qna findById(@Param("qnaNum") int qnaNum);
 }

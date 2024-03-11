b2c87d59f068c42367c7bdc451046a4c72b1d07c
kmc77 <k_minchoi@naver.com>
2024-03-11 19:57:45 +0900

    Player page tab 작업중

--- src/main/java/com/project/together/domain/K5_Player.java
+++ src/main/java/com/project/together/domain/K5_Player.java
@@ -6,7 +6,11 @@ import lombok.Data;
 public class K5_Player {
     private int k5PlayerNum;
     private String k5PlayerName;
+    private String k5PlayerEnName; //추가
     private String k5PlayerCapYn;
-    private String k5PlayerSubcapYn;
+    private String k5PlayerSubCapYn;
     private String k5PlayerPosition;
+    private String k5PlayerHeight; //추가
+    private String k5PlayerWeight; //추가
+    private String k5PlayerBirth;  //추가
 }

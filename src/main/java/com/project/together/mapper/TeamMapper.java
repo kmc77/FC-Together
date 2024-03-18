50e153a3d48a4f3ca9daee020a9148ab05873df9
kmc77 <k_minchoi@naver.com>
2024-03-18 19:59:21 +0900

    선수단 뷰페이지 작업중(포지션별 노출 완료)

--- src/main/java/com/project/together/mapper/TeamMapper.java
+++ src/main/java/com/project/together/mapper/TeamMapper.java
@@ -1,5 +1,7 @@
 package com.project.together.mapper;
 
+import com.project.together.domain.K5_Player;
+import com.project.together.domain.K7_Player;
 import com.project.together.domain.TeamStaff;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -8,4 +10,8 @@ import java.util.List;
 @Mapper
 public interface TeamMapper {
     TeamStaff getAllTeamStaff();
+
+    List<K5_Player> getK5Player();
+
+    List<K7_Player> getK7Player();
 }

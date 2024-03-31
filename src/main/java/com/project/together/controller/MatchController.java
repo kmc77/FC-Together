c98e4ab351de47be919fb42010e3ff1c287e1be2
twzt9983@naver.com <twzt9983@naver.com>
2024-03-31 19:16:22 +0900

    matchpage

--- src/main/java/com/project/together/controller/MatchController.java
+++ src/main/java/com/project/together/controller/MatchController.java
@@ -1,4 +1,21 @@
 package com.project.together.controller;
 
+import com.project.together.domain.Match;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class MatchController {
+
+
+@GetMapping
+@Transactional
+    public String matchResult(Model model, Match match) {
+
+    return "";
+    }
 }

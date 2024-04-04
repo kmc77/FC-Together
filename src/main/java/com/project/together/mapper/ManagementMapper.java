36395b46a76ecab03b349e37ea4f76a3f9b80f18
kmc77 <k_minchoi@naver.com>
2024-04-04 18:27:49 +0900

    경영공시 탭 목록 및 뷰페이지 완료

--- src/main/java/com/project/together/mapper/ManagementMapper.java
+++ src/main/java/com/project/together/mapper/ManagementMapper.java
@@ -1,6 +1,7 @@
 package com.project.together.mapper;
 
 import com.project.together.domain.File;
+import com.project.together.domain.Operation;
 import com.project.together.domain.Rule;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -12,14 +13,49 @@ import java.util.Map;
 
 @Mapper
 public interface ManagementMapper {
-    List<Rule> findAll(Map<String, Integer> params);
+
+    /* 임시 */
+    List<Rule> findAllRules(Map<String, Integer> params);
+
+    List<Rule> getRuleData();
+
+    List<Rule> findRules(@Param("offset") int offset, @Param("size") int size);
 
     Rule findRuleByRuleNum(int ruleNum);
 
     Rule findPrevRuleByCurrentRuleDate(LocalDate currentRuleDate);
 
-    void updateRuleHits(@Param("ruleNum") int ruleNum, @Param("hits") int hits);
+    void updateRuleHits(@Param("ruleNum") int ruleNum, @Param("ruleHits") int ruleHits);
 
     List<File> findFilesByRuleNum(int ruleNum);
+
+    int countRules();
+
+    // ================================== Rule End
+
+
+    // ================================== 경영공시 start
+
+    List<Operation> findAllOperations(Map<String, Integer> params);
+
+    List<Operation> findOperations(@Param("offset") int offset, @Param("size") int size);
+
+    Operation findOperationByRuleNum(int operationNum);
+
+
+    Operation findPrevOperationByCurrentOperationDate(LocalDate currentOperationDate);
+
+    void updateOperationHits(@Param("operationNum") int operationNum, @Param("operationHits") int operationHits);
+
+    List<File> findFilesByOperationNum(int operationNum);
+
+    int countOperations();
+
+
+    // ================================== 경영공시 End
+
+
+
+
 
 }

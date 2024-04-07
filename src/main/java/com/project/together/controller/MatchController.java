bc9140a3ddb9352afcdf5009554edaf734a2488e
twzt9983@naver.com <twzt9983@naver.com>
2024-04-08 01:00:03 +0900

    matchpage22

--- src/main/java/com/project/together/controller/MatchController.java
+++ src/main/java/com/project/together/controller/MatchController.java
@@ -1,4 +1,29 @@
 package com.project.together.controller;
 
+import com.project.together.domain.Match;
+import com.project.together.service.MatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class MatchController {
+
+    private final MatchService matchService;
+
+    @GetMapping("/match/matchpage")
+    public String matchResult(Model model) {
+        List<Match> matchResult = matchService.selectResult();
+        List<Match> matchSchedule = matchService.selectSchedule();
+        System.out.println("match" + matchResult);
+        System.out.println("match" + matchSchedule);
+        model.addAttribute("Resultlist", matchResult);
+        model.addAttribute("Schedulelist", matchSchedule);
+        return "layout/match/matchpage";
+    }
 }

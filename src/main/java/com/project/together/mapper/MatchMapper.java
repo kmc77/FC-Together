8a7688defc91ed3a5ebb137825b241050425ed69
twzt9983@naver.com <twzt9983@naver.com>
2024-04-07 18:36:52 +0900

    matchpage

--- src/main/java/com/project/together/mapper/MatchMapper.java
+++ src/main/java/com/project/together/mapper/MatchMapper.java
@@ -1,4 +1,13 @@
 package com.project.together.mapper;
 
+import com.project.together.domain.Match;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface MatchMapper {
+    List<Match> selectResult();
+
+    List<Match> selectSchedule();
 }

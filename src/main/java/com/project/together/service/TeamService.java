36f2ea09274a53c145c999a7a0e53c317c781f88
kmc77 <k_minchoi@naver.com>
2024-04-14 23:12:12 +0900

    팀소개 페이지 선수 사진 노출 처리완료

--- src/main/java/com/project/together/service/TeamService.java
+++ src/main/java/com/project/together/service/TeamService.java
@@ -6,7 +6,6 @@ import com.project.together.domain.TeamStaff;
 import com.project.together.domain.W1_Player;
 import com.project.together.mapper.TeamMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,16 +17,16 @@ public class TeamService {
     private final TeamMapper teamMapper;
 
 
-    public List<K5_Player> getK5Player() {
-        return teamMapper.getK5Player();
+    public List<K5_Player> getK5Players() {
+        return teamMapper.getK5Players();
     }
 
-    public List<K7_Player> getK7Player() {
-        return teamMapper.getK7Player();
+    public List<K7_Player> getK7Players() {
+        return teamMapper.getK7Players();
     }
 
-    public List<W1_Player> getW1Player() {
-        return teamMapper.getW1Player();
+    public List<W1_Player> getW1Players() {
+        return teamMapper.getW1Players();
     }
 
     public List<TeamStaff> getTeamStaff() {

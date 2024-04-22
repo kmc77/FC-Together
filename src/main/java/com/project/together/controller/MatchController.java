95d3df5b182791d73c8a08b341bf80d321b584c4
twzt9983@naver.com <twzt9983@naver.com>
2024-04-22 21:49:08 +0900

    matchpage

--- src/main/java/com/project/together/controller/MatchController.java
+++ src/main/java/com/project/together/controller/MatchController.java
@@ -18,18 +18,18 @@ public class MatchController {
 
     @GetMapping("/match/matchpage")
     public String matchResult(Model model) {
-        List<Match> K5matchResult = matchService.K5selectResult();
         List<Match> K5matchSchedule = matchService.K5selectSchedule();
-        List<Match> K7matchResult = matchService.K7selectResult();
+        List<Match> K5matchResult = matchService.K5selectResult();
         List<Match> K7matchSchedule = matchService.K7selectSchedule();
-        System.out.println("K5match" + K5matchResult);
+        List<Match> K7matchResult = matchService.K7selectResult();
         System.out.println("K5match" + K5matchSchedule);
-        System.out.println("K7match" + K7matchResult);
         System.out.println("K7match" + K7matchSchedule);
-        model.addAttribute("K5Resultlist", K5matchResult);
+        System.out.println("K5match" + K5matchResult);
+        System.out.println("K7match" + K7matchResult);
         model.addAttribute("K5Schedulelist", K5matchSchedule);
-        model.addAttribute("K7Resultlist", K7matchResult);
+        model.addAttribute("K5Resultlist", K5matchResult);
         model.addAttribute("K7Schedulelist", K7matchSchedule);
+        model.addAttribute("K7Resultlist", K7matchResult);
         return "layout/match/matchpage";
     }
 }

11287d1bbf49a494c4b0d475d1b9012e7b32cc6f
twzt9983@naver.com <twzt9983@naver.com>
2024-04-28 12:57:51 +0900

    a

--- src/main/java/com/project/together/controller/MatchController.java
+++ src/main/java/com/project/together/controller/MatchController.java
@@ -3,33 +3,34 @@ package com.project.together.controller;
 import com.project.together.domain.Match;
 import com.project.together.service.MatchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/match")
 public class MatchController {
 
     private final MatchService matchService;
 
-    @GetMapping("/match/matchpage")
-    public String matchResult(Model model) {
-        List<Match> K5matchSchedule = matchService.K5selectSchedule();
-        List<Match> K5matchResult = matchService.K5selectResult();
-        List<Match> K7matchSchedule = matchService.K7selectSchedule();
-        List<Match> K7matchResult = matchService.K7selectResult();
-        System.out.println("K5match" + K5matchSchedule);
-        System.out.println("K7match" + K7matchSchedule);
-        System.out.println("K5match" + K5matchResult);
-        System.out.println("K7match" + K7matchResult);
-        model.addAttribute("K5Schedulelist", K5matchSchedule);
-        model.addAttribute("K5Resultlist", K5matchResult);
-        model.addAttribute("K7Schedulelist", K7matchSchedule);
-        model.addAttribute("K7Resultlist", K7matchResult);
-        return "layout/match/matchpage";
+
+    @GetMapping("/layout/matches")
+    public ResponseEntity<Map<String, List<?>>> get_allPlayerInfo() {
+        Map<String, List<?>> allPlayersData = new HashMap<>();
+        allPlayersData.put("k5Schedule", matchService.K5selectSchedule());
+        allPlayersData.put("k5Result", matchService.K5selectResult());
+        allPlayersData.put("k7Schedule", matchService.K7selectSchedule());
+        allPlayersData.put("k7Result", matchService.K7selectResult());
+        allPlayersData.put("W1Schedule", matchService.W1selectSchedule());
+        allPlayersData.put("W1Result", matchService.W1selectResult());
+        System.out.println("컨트롤러 allPlayersData = " + allPlayersData);
+        return ResponseEntity.ok(allPlayersData);
     }
 }

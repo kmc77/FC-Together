b8cf280a93101aece34a68121387287a8fff3995
kmc77 <k_minchoi@naver.com>
2024-05-19 22:33:12 +0900

    MainPage 영상 작업

--- src/main/java/com/project/together/service/MainService.java
+++ src/main/java/com/project/together/service/MainService.java
@@ -1,4 +1,19 @@
 package com.project.together.service;
 
+import com.project.together.domain.ClubVideo;
+import com.project.together.mapper.MainMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MainService {
+
+    private final MainMapper mainMapper;
+
+    public List<ClubVideo> getAllClubVideos() {
+        return mainMapper.getAllClubVideos();
+    }
 }
